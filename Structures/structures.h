63b96abfe1527ddb90c6d5d5a77985a36ca046a0
aurelienlefeuvre <aurelien.lefeuvre@edu.ece.fr>
2022-11-09 11:04:43 +0100

    h

--- Structures/structures.h
+++ Structures/structures.h
@@ -1,6 +1,29 @@
 #ifndef ECE_CITY_1_E_STRUCTURES_H
 #define ECE_CITY_1_E_STRUCTURES_H
-
+#include <stdio.h>
+#define colonnes 45 //case
+#define ligne 35 //case
+#define hauteurResolution 1024 //pixel
+#define LargeurResolution 768 //pixel
+#define case 20 //pixel
+#define routeTaille 1 //case
+#define habitationTaille 3 //case
+#define batimentHauteur 4 //case
+#define batimentLargeur 6 //case
+#define capaciterAlimentation 5000 //pixel
+#define sommeInitiale 500000 //ECE-flouz
+#define cycle 15 //seconde
+#define terrainVague 0 //habitants
+#define ruine 0 //habitants
+#define cabane 10 //habitants
+#define maison 50 //habitants
+#define immeuble 100 //habitants
+#define gratteCiel 1000 //habitants
+#define prixCentrale 100000 //ECE-flouz
+#define prixChâteauDEau 100000 //ECE-flouz
+#define prixRoute 10 //ECE-flouz
+#define prixTerrainVague 1000 //ECE-flouz
+#define impots 10 //ECE-flouz
 #include <stdio.h>
 #include <stdlib.h>
 #include <time.h>
