b70e4adb354519e35aeac554d508aaca0267d574
Trystan <trystan.dubois@edu.ece.fr>
2022-11-11 10:26:51 +0100

    argent

---
+++ Fonctions/Argent/argent.c
@@ -0,0 +1,22 @@
+#include "argent.h"
+
+void retirerArgent(City * city){
+    // en mode graphique -> if click alors -payement batiment
+    for (int i = 0; i < LIGNES; i++) {
+        for (int j = 0; j < COLONNES; j++) {
+            if(city->terrain[i][j].typeBloc == 5){
+                city->argent -= city->ruine.prix;
+            }
+            if(city->terrain[i][j].typeBloc == 4){
+                city->argent -= city->chateauEau.prix;
+            }
+            if(city->terrain[i][j].typeBloc == 3){
+                city->argent -= city->centrale.prix;
+            }
+            if(city->terrain[i][j].typeBloc == 1){
+                city->argent -= city->route.prix;
+            }
+        }
+}
+
+    if cycle = true
---
+++ Fonctions/Argent/argent.h
@@ -0,0 +1,8 @@
+#ifndef ECE_CITY_1_E_ARGENT_H
+#define ECE_CITY_1_E_ARGENT_H
+#include "../../Structures/structures.h"
+
+
+void retirerArgent(City * city);
+
+#endif
