3934b9c4aa9cf85630110e3fa1e2cd2e8d76df07
Pierre-Louis <pierrelouis.thomas@edu.ece.fr>
2022-11-26 21:24:44 +0100

    sauvegarde (résolu)
    faire : menu en jeu

--- Fonctions/Sauvegarde/sauvegarde.c
+++ Fonctions/Sauvegarde/sauvegarde.c
@@ -6,8 +6,8 @@ void sauvegarderPartie (City *city, char* nomFichier) {
         printf("Erreur d'écriture dans le fichier\n");
         exit(-1);
     }
-    for (int i = 1; i < LIGNES; i++) {
-        for (int j = 1; j < COLONNES; j++) {
+    for (int i = 0; i < LIGNES; i++) {
+        for (int j = 0; j < COLONNES; j++) {
             fprintf(file, " %d", (city->terrain[i][j].typeBloc));
         }
         fprintf(file, "\n");
